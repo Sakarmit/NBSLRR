4ae8c746046683b410e12a081b48df7dceb6fdd3
Ninjajkl <ninjajkl@gmail.com>
2023-02-04 15:55:27 -0500

    Added Reset Button and Button Sound Effect

--- Assets/Code/GuiController.cs
+++ Assets/Code/GuiController.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartButton : MonoBehaviour
+public class GuiController : MonoBehaviour
 {
     [SerializeField] public Camera MainCamera;
+    [SerializeField] public AudioSource buttonSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +18,13 @@ public class StartButton : MonoBehaviour
         
     }
 
-    public void Pressed()
+    public void PanCameraDown()
     {
         MainCamera.GetComponent<MainCamera>().CameraSmoothMove2D(new Vector2(0, 6.72f), new Vector2(0, 0));
+    }
+
+    public void PlayButtonSound()
+    {
+        buttonSound.Play();
     }
 }
