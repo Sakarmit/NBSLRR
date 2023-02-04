f4eb45cd692803de6e87aa87a12323ac64192db1
CaptainIndia <ayirakas28@gmail.com>
2023-02-03 22:32:22 -0500

    Movement System

--- Assets/Code/BadSprite.cs
+++ Assets/Code/BadSprite.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class BadSprite : BaseSprite
 {
+    [SerializeField] private float node = 99f;
+    private GameObject targetNode;
 
+    private float movementSpeed = 1f;
+
+    private void Start()
+    {
+        targetNode = GameObject.Find("PathNode (" + node + ")");
+    }
+    private void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetNode.transform.position, Time.deltaTime * movementSpeed);
+        if (transform.position == targetNode.transform.position)
+        {
+            if (node == 0)
+            {
+                Destroy(gameObject);
+                Debug.LogWarning("***Insert Player Base Damage Here***");
+            }
+            targetNode = GameObject.Find("PathNode (" + --node + ")");
+        }
+    }
 }
--- Assets/Code/GoodSprite.cs
+++ Assets/Code/GoodSprite.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class GoodSprite : BaseSprite
 {
+    [SerializeField] private float node = 0;
+    [SerializeField] private float LastNode = 0;
+    private GameObject targetNode;
 
+    private float movementSpeed = 1f;
+
+    private void Start()
+    {
+        targetNode = GameObject.Find("PathNode (" + node + ")");
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log(gameObject.name);
+        transform.position = Vector2.MoveTowards(transform.position, targetNode.transform.position, Time.deltaTime * movementSpeed);
+        if (transform.position == targetNode.transform.position)
+        {
+            if (node == LastNode)
+            {
+                Destroy(gameObject);
+                Debug.LogWarning("***Insert Ememy Base Damage Here***");
+            }
+            targetNode = GameObject.Find("PathNode (" + ++node + ")");
+        }
+    }
 }
