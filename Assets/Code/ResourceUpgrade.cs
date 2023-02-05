794f024519c1c7fd2c8fec4fb56b699dcff5e632
CaptainIndia <ayirakas28@gmail.com>
2023-02-05 16:31:58 -0500

    wksbfkawebf

--- Assets/Code/ResourceUpgrade.cs
+++ Assets/Code/ResourceUpgrade.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -11,6 +12,12 @@ public class ResourceUpgrade : MonoBehaviour
     int costScaler;
     [SerializeField] PlayerBase playerBase;
 
+    [SerializeField] TextMeshProUGUI text;
+
+    private void Start()
+    {
+        text.text = "Cost: " + startUpgradeCost.ToString();
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -26,6 +33,7 @@ public class ResourceUpgrade : MonoBehaviour
             playerBase.resourceRate++;
             upgradeCost *= costScaler;
             GetComponent<AudioSource>().Play();
+            text.text = "Cost: " + upgradeCost.ToString();
         }
     }
     public void SetDefaults()
